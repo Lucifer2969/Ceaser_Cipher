847e2413f3f41f3a14a8c2c12b37944af1f238f7
Lucifer <raghv.afgji@icloud.com>
2025-01-07 19:15:35 +0530

    Basic coding complete

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-alphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
-             'w','x','y','z'}
+alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+             'w','x','y','z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to dcrypt the message\n").lower()
+text = input("Enter the message\n").lower()
+shift = int(input("Type the shift number.\n"))
+
+def ceaser(original_text, shift_amount, ceaser_text):
+    cypher_text = ''
+    for letter in original_text:
+        if letter == ' ':
+            cypher_text += ' '
+            continue
+    if ceaser_text == "decode":
+        shift_amount *= -1
+    value = alphabets.index(letter) + shift_amount
+    value %= len(alphabets)
+    cypher_text += alphabets[value]
+    print(cypher_text)     
+     
+
+ceaser(text, shift, direction)
