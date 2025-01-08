74823ca5251b8b61f507ac745bc6d4f22bd2935e
Lucifer <raghv.afgji@icloud.com>
2025-01-08 12:20:23 +0530

    Final code changes

--- main.py
+++ main.py
@@ -6,28 +6,29 @@ alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 print(logo)
 condition = False
 
-while not condition:
 
+def ceaser(original_text, shift_amount, ceaser_text):
+    cypher_text = ''
+    if ceaser_text == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabets:
+            cypher_text += letter
+            continue
+        value = alphabets.index(letter) + shift_amount
+        value %= len(alphabets)
+        cypher_text += alphabets[value]
+    print(f"Here is the {direction}d result, {cypher_text}.")
+
+
+while not condition:
     direction = input("Type 'encode' to encrypt, type 'decode' to dcrypt the message\n").lower()
     text = input("Enter the message\n").lower()
     shift = int(input("Type the shift number.\n"))
 
-
-    def ceaser(original_text, shift_amount, ceaser_text):
-        cypher_text = ''
-        for letter in original_text:
-            if letter not in alphabets:
-                cypher_text += letter
-                continue
-            if ceaser_text == "decode":
-                shift_amount *= -1
-            value = alphabets.index(letter) + shift_amount
-            value %= len(alphabets)
-            cypher_text += alphabets[value]
-        print(f"Here is the {direction}d result, {cypher_text}.")
-
-
     ceaser(text, shift, direction)
-    cont_conditon = input("Press 'yes' if you want to continue again.\n")
+
+    cont_conditon = input("Press 'yes' if you want to continue again and 'no' to exit.\n").lower()
+
     if cont_conditon != 'yes':
         condition = True
