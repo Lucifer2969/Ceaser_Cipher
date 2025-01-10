25d3f2f35bff563afd3663f414fa4dbc66fea389
Loki420-a <sahil1705103@gmail.com>
2025-01-10 16:04:39 +0530

    Adding Comments

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 print(logo)
 condition = False
 
-
+# Function Defination -- Inside the function as per the given conditional loop Encoding and Decoding happens with the help of shift and output is 
+# printed as result.
 def ceaser(original_text, shift_amount, ceaser_text):
     cypher_text = ''
     if ceaser_text == "decode":
@@ -21,7 +22,7 @@ def ceaser(original_text, shift_amount, ceaser_text):
         cypher_text += alphabets[value]
     print(f"Here is the {direction}d result, {cypher_text}.")
 
-
+# Condition While to get the inputs from the User to get the shift_amount,text and direction(weather the user want's to get input as Encode or Decode).
 while not condition:
     direction = input("Type 'encode' to encrypt, type 'decode' to dcrypt the message\n").lower()
     text = input("Enter the message\n").lower()
@@ -29,6 +30,7 @@ while not condition:
 
     ceaser(text, shift, direction)
 
+# Take user Input if 'YES'to repeat with the same process if 'NO' program ends.
     cont_conditon = input("Press 'yes' if you want to continue again and 'no' to exit.\n").lower()
 
     if cont_conditon != 'yes':
